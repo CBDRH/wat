a097287b5c4fd090044af953837c5065505ba7fc
leiboliu <17636764+leiboliu@users.noreply.github.com>
2020-11-25 13:01:36 +1100

    Add files via upload

---
+++ build/precache-manifest.503f2d080691a90b9aaaca89657f8e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a17da8254d96fa3cd64a77a8dcf6ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427d58f909c3ade6861b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9e05d3907fb4d95fb14d",
+    "url": "/static/css/main.75b3bf04.chunk.css"
+  },
+  {
+    "revision": "427d58f909c3ade6861b",
+    "url": "/static/js/2.96369dfe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.96369dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e05d3907fb4d95fb14d",
+    "url": "/static/js/main.d367deb2.chunk.js"
+  },
+  {
+    "revision": "fe884d370427d5e31276",
+    "url": "/static/js/runtime-main.3608c4a6.js"
+  }
+]);
